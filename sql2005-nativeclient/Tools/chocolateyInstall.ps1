8031d3ba81bf7dcf1b846157af1224cac1bceb09
Ritch Melton <skyguy94@gmail.com>
2015-01-09 22:41:01 -0600

    Busywork

--- sql2005-nativeclient/Tools/chocolateyInstall.ps1
+++ sql2005-nativeclient/Tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-Install-ChocolateyPackage 'sql2005-nativeclient' 'msi' '/quiet' 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/sqlncli.msi' 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/sqlncli_x64.msi' @(0, 1603)
+$packageName = 'sql2005-nativeclient'
+$setupType = 'msi'
+$setupArgs = '/quiet'
+$32bitUrl = 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/sqlncli.msi'
+$64bitUrl = 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/sqlncli_x64.msi'
+
+Install-ChocolateyPackage $packageName $setupType $setupArgs $32bitUrl $64bitUrl
--- sql2005-xmo/Tools/chocolateyInstall.ps1
+++ sql2005-xmo/Tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-Install-ChocolateyPackage 'sql2005-xmo' 'msi' '/quiet' 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/SQLServer2005_XMO.msi' 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/SQLServer2005_XMO_x64.msi' @(0, 1603)
+$packageName = 'sql2005-xmo'
+$setupType = 'msi'
+$setupArgs = '/quiet'
+$32bitUrl = 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/SQLServer2005_XMO.msi'
+$64bitUrl = 'http://download.microsoft.com/download/3/1/6/316FADB2-E703-4351-8E9C-E0B36D9D697E/SQLServer2005_XMO_x64.msi'
+
+Install-ChocolateyPackage $packageName $setupType $setupArgs $32bitUrl $64bitUrl
