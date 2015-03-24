88c6f0072b99fc8fc6574b7d32f7e06a641645da
skyguy94 <skyguy94@gmail.com>
2015-03-24 16:46:00 -0500

    Its service name not display name

--- tomcat/tools/chocolateyInstall.ps1
+++ tomcat/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $checksumType = 'md5'
 $checksum64 = 'a4121b78c8eb12c7af0b7fad6fec39d6'
 $checksumType64 = 'md5'
 $global:availablePort = '8080'
-$global:serviceName = 'Apache Tomcat 7.0 Tomcat7'
+$global:serviceName = 'Tomcat7'
 
 if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\OverwriteParameters.ps1"
@@ -19,8 +19,8 @@ Set-Content -Path (Join-Path $PSScriptRoot "install-options.ps1") -Value "$globa
 Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$global:installLocation" "$64BitUrl" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
 
 $catalinaHome = "$global:installLocation\Apache Tomcat 7.0.59"
-$createServiceCommand = "${catalinaHome}\bin\service.bat install $packageName"
+$createServiceCommand = "${catalinaHome}\bin\service.bat install $global:serviceName"
 
 Install-ChocolateyEnvironmentVariable 'CATALINA_HOME' "$catalinaHome"
 
-Install-Service $packageName "$global:serviceName" $createServiceCommand "$global:availablePort"
+Install-Service $packageName $global:serviceName $createServiceCommand $global:availablePort
