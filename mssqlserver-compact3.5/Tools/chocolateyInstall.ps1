e97f9b143c527bd3c2339d466dfb453c03867d1f
skyguy94 <skyguy94@gmail.com>
2015-05-30 12:39:40 -0500

    improvements

--- mssqlserver-compact3.5/Tools/chocolateyInstall.ps1
+++ mssqlserver-compact3.5/Tools/chocolateyInstall.ps1
@@ -1,17 +1,41 @@
-$packageName = 'SSCERuntime-ENU'
-$fileType = 'exe'
-$tempDir = (Join-Path (Join-Path $env:TEMP "chocolatey") "$packageName")
-
-if (Test-Path $tempDir) {
-  Remove-Item -Recurse -Force $tempDir
+$options = @{
+  tempDir = Join-Path ([System.IO.Path]::GetTempPath()) ([System.IO.Path]::GetRandomFileName())
 }
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
 
-$file = Join-Path $tempDir "$($packageName)Install.$fileType"
-Get-ChocolateyWebFile 'SSCERuntime-ENU' $file 'http://download.microsoft.com/download/E/C/1/EC1B2340-67A0-4B87-85F0-74D987A27160/SSCERuntime-ENU.exe'
+$packageParameters = @{
+  packageName = 'SSCERuntime-ENU';
+  fileFullPath = Join-Path $options['tempDir'] "SSCERuntime-ENUInstall.exe";
+  url = 'http://download.microsoft.com/download/E/C/1/EC1B2340-67A0-4B87-85F0-74D987A27160/SSCERuntime-ENU.exe';
+  url64bit = 'http://download.microsoft.com/download/E/C/1/EC1B2340-67A0-4B87-85F0-74D987A27160/SSCERuntime-ENU.exe';
+  checksum = '';
+  checksumType = '';
+  checksum64 = '';
+  checksumType64 = '';
+}
 
-Invoke-Expression "$file /T:$tempDir /q"
-Install-ChocolateyPackage 'mssqlserver-compact3.5-x32' 'msi' '/quiet /passive' (Join-Path $tempDir 'SSCERuntime_x86-ENU.msi') '' @(0,1603)
-if (Get-ProcessorBits -eq 64) {
-    Install-ChocolateyPackage 'mssqlserver-compact3.5-x64' 'msi' '/quiet /passive' '' (Join-Path $tempDir 'SSCERuntime_x64-ENU.msi') @(0,1603)
+$packageParameters
+
+try {
+  if (![System.IO.Directory]::Exists($options['tempDir'])) { [System.IO.Directory]::CreateDirectory($options['tempDir']) | Out-Null }
+
+  Get-ChocolateyWebFile @packageParameters
+
+  Start-Process "$($packageParameters['fileFullPath'])" -ArgumentList "/T:$($options['tempDir']) /q" -Wait
+
+  try {
+    Install-ChocolateyPackage 'mssqlserver-compact3.5-x32' 'msi' '/quiet /passive' (Join-Path $options['tempDir'] 'SSCERuntime_x86-ENU.msi') ''
+  }
+  catch {
+    #Try to handle a partially installed condition where x86 is installed but x64 isn't.
+    if ($LASTEXITCODE -ne 1603) { throw $_ }
+  }
+
+  if (Get-ProcessorBits -eq 64) {
+      Install-ChocolateyPackage 'mssqlserver-compact3.5-x64' 'msi' '/quiet /passive' '' (Join-Path $options['tempDir'] 'SSCERuntime_x64-ENU.msi')
+  }
+}
+Finally {
+  if (Test-Path $options['tempDir']) {
+    Remove-Item -Recurse -Force $options['tempDir']
+  }
 }
