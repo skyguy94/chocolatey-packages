1e1f52d8685444798198226c95ebdf6dc1ccec48
skyguy94 <skyguy94@gmail.com>
2015-10-22 16:36:32 -0500

    update url

--- visualstudio2015-modelingsdk/Tools/chocolateyInstall.ps1
+++ visualstudio2015-modelingsdk/Tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'visualstudio2015-modelingsdk'
-$downloadUrl = 'http://download.microsoft.com/download/A/6/5/A6527B55-2836-42F3-B22E-49A4C96E01B0/vs_vmsdk.exe'
-
+$downloadUrl = 'https://download.microsoft.com/download/0/B/0/0B05CE52-5C57-46E9-8C27-C5C4DF10C8C0/vs_vmsdk.exe'
+                
 $logFilePath = Join-Path $env:TEMP 'vs_vmsdk.log'
 $installArgs = '/Passive /NoRestart /Log ' + $logFilePath
 Install-ChocolateyPackage $packageName 'exe' $installArgs $downloadUrl -validExitCodes @(0,3010)
