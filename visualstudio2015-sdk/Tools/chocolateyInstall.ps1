8c3f7b40684e3a0439c3db598322035b3da03980
skyguy94 <skyguy94@gmail.com>
2015-10-22 16:44:44 -0500

    Fix url and bump version

--- visualstudio2015-sdk/Tools/chocolateyInstall.ps1
+++ visualstudio2015-sdk/Tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'visualstudio2015-sdk'
-$downloadUrl = 'http://download.microsoft.com/download/1/9/4/194D9E3A-3E78-421A-8A28-30A3D9A52A46/vssdk_full.exe'
+$downloadUrl = 'http://download.microsoft.com/download/9/1/0/910EE61D-A231-4DAB-BD56-DCE7092687D5/vssdk_full.exe'
 
 $logFilePath = Join-Path $env:TEMP 'vssdk_full.log'
 $installArgs = '/Passive /NoRestart /Log ' + $logFilePath
