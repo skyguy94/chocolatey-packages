87cc5beed035aaaf855d1b94749bef8db468dd72
skyguy94 <skyguy94@gmail.com>
2015-11-12 18:35:29 -0600

    Rolled into the VS primary installer

--- visualstudio2015-sdk/Tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = 'visualstudio2015-sdk'
-$downloadUrl = 'http://download.microsoft.com/download/9/1/0/910EE61D-A231-4DAB-BD56-DCE7092687D5/vssdk_full.exe'
-
-$logFilePath = Join-Path $env:TEMP 'vssdk_full.log'
-$installArgs = '/Passive /NoRestart /Log ' + $logFilePath
-Install-ChocolateyPackage $packageName 'exe' $installArgs $downloadUrl -validExitCodes @(0,3010)
