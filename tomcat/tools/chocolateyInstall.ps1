72ca447818fcc9cb38c6680191c1d47f30d53515
skyguy94 <skyguy94@gmail.com>
2016-04-24 09:38:20 -0500

    tomcat7

--- tomcat/tools/chocolateyInstall.ps1
+++ tomcat/tools/chocolateyInstall.ps1
@@ -1,18 +1,18 @@
 ﻿$PFFolder = if (Get-ProcessorBits -eq 64) { "$Env:ProgramFiles" } else { "$Env:ProgramFiles(x86)" };
 
 $options = @{
-    version = '8.0.32';
+    version = '7.0.69';
     unzipLocation = (Join-Path $PFFolder "Apache Software Foundation\tomcat");
-    serviceName = 'Tomcat8';
+    serviceName = 'Tomcat7';
 }
 
 $unzipParameters = @{
     packageName = 'tomcat';
-    url = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x86.zip";
-    url64bit = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x64.zip";
-    checksum = '1393f5cd080f14963fbd5dcf9741b7bf';
+    url = "https://archive.apache.org/dist/tomcat/tomcat-7/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x86.zip";
+    url64bit = "https://archive.apache.org/dist/tomcat/tomcat-7/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x64.zip";
+    checksum = 'ce764b8bd89e4c142c460ad8e6ff8b08';
     checksumType = 'md5';
-    checksum64 = '148d9deab641de732769f1ed6db4dff1';
+    checksum64 = '1cafbe7fe52f7da6882d1aa8e1febd4a';
     checksumType64 = 'md5';
 }
 
@@ -23,6 +23,7 @@ Set-ChocolateyPackageOptions $options
 Install-ChocolateyZipPackage @unzipParameters -UnzipLocation $options['unzipLocation']
 
 $catalinaHome = Join-Path $options['unzipLocation'] "apache-tomcat-$($options['version'])";
+Install-ChocolateyEnvironmentVariable 'CATALINA_HOME' "$catalinaHome"
 $binPath = Join-Path $catalinaHome 'bin'
 
 Push-Location $binPath
