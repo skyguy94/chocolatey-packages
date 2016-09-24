880d8a24032d188cffa1b0daff9d04c9b5de447c
skyguy94 <skyguy94@gmail.com>
2016-09-24 06:51:19 -0500

    update teamcity to latest

--- teamcity/tools/chocolateyInstall.ps1
+++ teamcity/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$options = @{
-  version = '10.0.1';
+  version = '10.0.2';
   unzipLocation = 'C:\';
   runAsSystem = $true;
   serviceName = 'TeamCity';
@@ -11,8 +11,8 @@ $packageParameters = @{
   packageName = 'teamcity';
   url = "http://download.jetbrains.com/teamcity/TeamCity-$($options['version']).tar.gz";
   url64bit = '';
-  checksum = '';
-  checksumType = '';
+  checksum = '35ABB03ED176C8326ADC86CAC17A93412C7248277D9AAE422B89BE17EDFF8F97';
+  checksumType = 'Sha256';
   checksum64 = '';
   checksumType64 = '';
 }
