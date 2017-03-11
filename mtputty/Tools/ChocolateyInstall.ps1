a1047b383e0c74b04a1184a31e0003952373d0aa
Ritch Melton <skyguy94@gmail.com>
2017-03-11 11:22:02 -0600

    fix checksum and bump version

--- mtputty/Tools/ChocolateyInstall.ps1
+++ mtputty/Tools/ChocolateyInstall.ps1
@@ -1,6 +1,9 @@
-$packageName = 'mtputty'
-$toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
-$localFile = Join-Path $toolsPath "mtputty.exe"
-$url = 'http://ttyplus.com/download/mtputty.exe'
+$packageParameters = @{
+  PackageName = 'mtputty'
+  FileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\mtputty.exe"
+  Url = 'http://ttyplus.com/download/mtputty.exe'
+  Checksum = 'C2A81F5DB114ABB5EC44ECA5F69FA6F93723A272B233F313EB363E411CD52069'
+  ChecksumType = 'Sha256'
+}
 
-Get-ChocolateyWebFile $packageName $localFile $url
+Get-ChocolateyWebFile @packageParameters
