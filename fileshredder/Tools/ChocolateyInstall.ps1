302a8c02fd24cacede107c7e3af47adb0311d401
Ritch Melton <skyguy94@gmail.com>
2017-03-11 11:15:56 -0600

    fix checksum and bump version

--- fileshredder/Tools/ChocolateyInstall.ps1
+++ fileshredder/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,11 @@
-$packageName = 'fileshredder'
-$setupType = 'exe'
-$setupArgs = '/SILENT'
-$32bitUrl = 'http://www.fileshredder.org/files/file_shredder_setup.exe'
-$64bitUrl = ''
+$packageParameters = @{
+  PackageName = 'fileshredder'
+  SetupType = 'exe'
+  SilentArgs = '/SILENT'
+  Url = 'http://www.fileshredder.org/files/file_shredder_setup.exe'
+  Checksum = '72714927DE74B97C524C5FA8BC1A0DEC83F038DBBED80B93B5E6280CA1317F41'
+  ChecksumType = 'Sha256'
+}
 
-Install-ChocolateyPackage $packageName $setupType $setupArgs $32bitUrl $64bitUrl
+Install-ChocolateyPackage @packageParameters
 
