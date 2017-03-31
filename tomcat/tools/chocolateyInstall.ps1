885880430aa2b603c1edb0dd24b051e83df4716f
Ritch Melton <skyguy94@gmail.com>
2017-03-31 09:07:45 -0500

    whatever

--- tomcat/tools/chocolateyInstall.ps1
+++ tomcat/tools/chocolateyInstall.ps1
@@ -1,19 +1,19 @@
-﻿$PFFolder = if (Get-ProcessorBits -eq 64) { "$Env:ProgramFiles" } else { "$Env:ProgramFiles(x86)" };
+﻿$PFFolder = if (Get-ProcessorBits -eq 64) { "$Env:ProgramFiles" } else { "$Env:ProgramFiles(x86)" }
 
 $options = @{
-    version = '8.5.12';
-    unzipLocation = (Join-Path $PFFolder "Apache Software Foundation\tomcat");
-    serviceName = 'Tomcat8';
+    version = '8.5.12'
+    unzipLocation = (Join-Path $PFFolder "Apache Software Foundation\tomcat")
+    serviceName = 'Tomcat8'
 }
 
 $unzipParameters = @{
-    packageName = 'tomcat';
-    url = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x86.zip";
-    url64bit = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x64.zip";
-    checksum = '151ecf591ec0264f9c2ac056373537e13cba2680';
-    checksumType = 'sha1';
-    checksum64 = '814e96e6a4d3bc124b93a8314506fac933d28c2f';
-    checksumType64 = 'sha1';
+    packageName = 'tomcat'
+    url = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x86.zip"
+    url64bit = "https://archive.apache.org/dist/tomcat/tomcat-8/v$($options['version'])/bin/apache-tomcat-$($options['version'])-windows-x64.zip"
+    checksum = '151ecf591ec0264f9c2ac056373537e13cba2680'
+    checksumType = 'sha1'
+    checksum64 = '814e96e6a4d3bc124b93a8314506fac933d28c2f'
+    checksumType64 = 'sha1'
 }
 
 if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
@@ -25,7 +25,8 @@ Install-ChocolateyZipPackage @unzipParameters -UnzipLocation $options['unzipLoca
 $catalinaHome = Join-Path $options['unzipLocation'] "apache-tomcat-$($options['version'])";
 Install-ChocolateyEnvironmentVariable 'CATALINA_HOME' "$catalinaHome"
 
-Push-Location (Join-Path $catalinaHome 'bin')
+$binPath =  Join-Path $catalinaHome 'bin'
+Push-Location $binPath
 Start-ChocolateyProcessAsAdmin ".\service.bat install $($options['serviceName'])"
 Pop-Location
 
