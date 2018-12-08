38d51470519632c342a590d8d54be8fc595247b4
David Gardiner <david@gardiner.net.au>
2018-12-08 13:44:45 +1030

    TeamCity 2018.1.4

--- teamcity/tools/chocolateyInstall.ps1
+++ teamcity/tools/chocolateyInstall.ps1
@@ -7,14 +7,13 @@
   domain = '';
   password = '';
 }
+
+# https://download.jetbrains.com/teamcity/TeamCity-2018.1.4.tar.gz.sha256 
 $packageParameters = @{
   packageName = 'teamcity';
   url = "https://download.jetbrains.com/teamcity/TeamCity-$($options['version']).tar.gz";
-  url64bit = '';
-  checksum = 'd16ec0a4693cf4203d5e201cedf00e7a1559efaec98f54d018e67a3a17ad893d';
+  checksum = 'f6d4df1eb1800b5016e79f3fc58d6f5c926bbd65b54ccb20009a1371b6eb89ee';
   checksumType = 'Sha256';
-  checksum64 = '';
-  checksumType64 = '';
 }
 
 if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
