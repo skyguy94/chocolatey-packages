9c36de4807e08d13ddfd343c041c4fd235ef9618
Ritch Melton <ritch@careevolution.com>
2019-05-15 08:59:29 -0500

    update

--- iiscrypto/Tools/ChocolateyInstall.ps1
+++ iiscrypto/Tools/ChocolateyInstall.ps1
@@ -3,6 +3,6 @@ $toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $localFile = Join-Path $toolsPath "iiscrypto.exe"
 $localFile40 = Join-Path $toolsPath "iiscrypto40.exe"
 
-Get-ChocolateyWebFile $packageName $localFile 'https://www.nartac.com/Products/IISCrypto/IISCrypto.exe'
-Get-ChocolateyWebFile $packageName $localFile40 'https://www.nartac.com/Products/IISCrypto/IISCrypto40.exe'
+Get-ChocolateyWebFile $packageName $localFile 'https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe'
+Get-ChocolateyWebFile $packageName $localFile40 'https://www.nartac.com/Downloads/IISCrypto/IISCrypto40.exe'
 
