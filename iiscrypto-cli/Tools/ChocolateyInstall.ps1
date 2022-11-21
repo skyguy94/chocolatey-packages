ce68c2977b79a2bbb07f2d7a12942e69973124cf
Ritch Melton <skyguy94@gmail.com>
2022-11-21 15:56:49 +0100

    update IISCrypto

--- iiscrypto-cli/Tools/ChocolateyInstall.ps1
+++ iiscrypto-cli/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,6 @@
 $packageName = 'iiscrypto-commandline'
 $toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $localFile = Join-Path $toolsPath "iiscryptocli.exe"
-$localFile40 = Join-Path $toolsPath "iiscryptocli40.exe"
 
 Get-ChocolateyWebFile $packageName $localFile 'https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe'
-Get-ChocolateyWebFile $packageName $localFile40 'https://www.nartac.com/Products/IISCrypto/IISCryptoCli40.exe'
 
--- iiscrypto/Tools/ChocolateyInstall.ps1
+++ iiscrypto/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,6 @@
 $packageName = 'iiscrypto-commandline'
 $toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $localFile = Join-Path $toolsPath "iiscrypto.exe"
-$localFile40 = Join-Path $toolsPath "iiscrypto40.exe"
 
 Get-ChocolateyWebFile $packageName $localFile 'https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe'
-Get-ChocolateyWebFile $packageName $localFile40 'https://www.nartac.com/Downloads/IISCrypto/IISCrypto40.exe'
 
